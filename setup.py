c5f79a5ba0c8f21c3ebb318b943f078e6010c922
SCC-github <63506287+SCC-github@users.noreply.github.com>
2022-04-21 14:12:12 +0100

    Update setup.py

--- setup.py
+++ setup.py
@@ -1,21 +1,13 @@
 # -*- coding: utf-8 -*-
-
-from worxcloud_sc import *
 import setuptools
-
-requirements = ['paho-mqtt==1.6.1',
-                'pyOpenSSL==21.0.0',
-                'ratelimit==2.2.1']
-
 
 setuptools.setup(
     name = 'worxcloud_sc',\
-#    version = __version__
+    version="1.4.15",
     description = 'Worx Landroid API library',
     author = 'Morten Trab',
     author_email = 'morten@trab.dk',
     license= 'MIT',
     url = 'https://github.com/SCC-github/worxcloud_sc',
     packages=setuptools.find_packages(),
-    install_requires=requirements,
 )
